b51e2cb841bd923175a1bb2e581fec30eec47133
Mr. Lin <cufoon@qq.com>
2023-06-26 01:07:04 +0800

    feat: 从 package.json 读取版本号

--- script/makebin.js
+++ script/makebin.js
@@ -7,16 +7,20 @@ const nowDate = new Date();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const packagePath = resolve(__dirname, '../package.json');
 const appFilePath = resolve(__dirname, '../build/app.js');
 const appInfoFilePath = resolve(__dirname, '../build/version.js');
 const appInfoFileTsPath = resolve(__dirname, '../build/version.d.ts');
+
+const packageInfo = JSON.parse(fs.readFileSync(packagePath).toString());
+const appVersion = packageInfo.version;
 
 const dataApp = fs.readFileSync(appFilePath);
 
 fs.writeFileSync(appFilePath, '#!/usr/bin/env node\n');
 fs.appendFileSync(appFilePath, dataApp);
 
-const nowVersion = `LitAES v1.0.5(${nowDate.toLocaleDateString()} ${nowDate.toLocaleTimeString()})`;
+const nowVersion = `LitAES v${appVersion}(${nowDate.toLocaleDateString()} ${nowDate.toLocaleTimeString()})`;
 const dataAppInfoPlaceholder = fs.readFileSync(appInfoFilePath).toString();
 const dataAppInfo = dataAppInfoPlaceholder.replace(
   'cufoon_application_version_info_placehodler',
